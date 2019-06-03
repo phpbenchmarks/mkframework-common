e3f6807e22ee4c5b1121b0cd114604511a79a8a3
mika <imikado@gmail.com>
2019-06-03 21:27:45 +0200

    ajout fichier de conf

---
+++ conf/routing.php
@@ -0,0 +1,12 @@
+<?php
+$tab=array(
+		'benchmark/helloworld' => array(
+						'nav'=>'global_default::index',
+
+						),
+			//page 404 (page non trouve)
+			'404' => array(
+								'nav' => 'article::list',
+								),
+
+		);
