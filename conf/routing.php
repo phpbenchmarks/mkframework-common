3d05a620ad9f36efc4d5916dfb6d0449937930dd
mika <imikado@gmail.com>
2019-06-24 09:30:40 +0200

    fix

--- conf/routing.php
+++ conf/routing.php
@@ -1,12 +1,11 @@
 <?php
 $tab=array(
-		'helloworld' => array(
-						'nav'=>'global_default::index',
+	'helloworld' => array(
+		'nav'=>'global_default::index',
+	),
+	//page 404 (page non trouve)
+	'404' => array(
+		'nav' => 'article::list',
+	),
 
-						),
-			//page 404 (page non trouve)
-			'404' => array(
-								'nav' => 'article::list',
-								),
-
-		);
+);
